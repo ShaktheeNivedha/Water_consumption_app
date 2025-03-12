2be6bd7bba6d0408adf690ca8ac8100e44606c97
ShaktheeNivedha <shaktheenivedha@gmail.com>
2025-03-12 16:34:30 +0530

    initial commit

--- app.py
+++ app.py
@@ -11,19 +11,15 @@ scaler = joblib.load('scaler.pkl')
 st.title("🚰 Water Consumption Prediction App")
 st.markdown("This app predicts future water consumption using AI models.")
 
-
 date_input = st.date_input("Select a Date for Prediction:", pd.to_datetime("2025-01-01"))
 num_days = st.slider("Predict for how many days?", 1, 30, 7)
 
 future_dates = pd.date_range(start=date_input, periods=num_days, freq='D')
 
-
 X_future = np.random.rand(len(future_dates), 7)
 X_future_scaled = scaler.transform(X_future)
 
-
 rf_preds = rf_model.predict(X_future_scaled)
-
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=future_dates, y=rf_preds, mode='lines', name='Random Forest', line=dict(color='red', dash='dash')))
